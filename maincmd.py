b481ca6adade9f9f52505232f79b25b54274f50a
CIUser <CIUser@veeroute.com>
2023-02-11 16:29:29 +0300

    cmd

--- maincmd.py
+++ maincmd.py
@@ -2,6 +2,7 @@
 from devices import Devices
 from sensors import Sensors
 devices = Devices()
+import time
 
 print("devices:")
 for device in devices.get_devices():
@@ -23,6 +24,20 @@ for name in sensors.get_sensors_names():
     sensor_value = sensor.get_value()
     print(f"sensor name: {name}, channel: {channel}, input: {pin_value}, value: {sensor_value}")
 
+
+for i in range(0,100):
+    name = "oil_pressure"
+    sensor = sensors.get_sensor(name)
+    channel = sensor.get_channel()
+    pin = devices.get_pin_by_channel(channel=channel)
+    pin_value = pin.get_value()
+
+    sensor.set_input(pin_value)
+    sensor.calculate()
+
+    sensor_value = sensor.get_value()
+    print(f"sensor name: {name}, channel: {channel}, input: {pin_value}, value: {sensor_value}")
+    time.sleep(1)
 
 #obd = Obd()
 #obd.loop()
